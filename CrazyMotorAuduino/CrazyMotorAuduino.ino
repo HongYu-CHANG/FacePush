c7c0610157bd48ce31050b932dde21d587465605
RedFishChang <harris32916@gmail.com>
2017-11-20 13:44:21 +0800

    Arduino can read degree

--- CrazyMotorAuduino/CrazyMotorAuduino.ino
+++ CrazyMotorAuduino/CrazyMotorAuduino.ino
@@ -1,17 +1,3 @@
-/*
- * Easing
- * Tobias Toft <hello@tobiastoft.dk>
- * July 27, 2009
- *
- * Moves a servo motor back and forth between 0 and 140 degrees
- * when a button is pressed.
- *
- * This example is using the Servo.h library that comes
- * with the Arduino IDE.
- *
- * Easing functions based on Robert Penner's work,
- * for more info see Easing.h or Easing.cpp
- */
 #include <Servo.h>
 
 Servo rightServo; //create servo object
@@ -21,10 +7,10 @@ void setup()
 {
   Serial.begin(115200);
   
-  rightServo.attach(13); //attach servo at pin 13
-  rightServo.write(180);  //put servo at 0 degrees
-  leftServo.attach(7); //attach servo at pin 8
-  leftServo.write(0);  //put servo at 140 degrees
+  rightServo.attach(11,500,2500); //attach servo at pin 13
+  rightServo.write(0);
+  leftServo.attach(6,500,2500); //attach servo at pin 8
+  leftServo.write(150);
   Serial.println("--- Start Serial Monitor SEND_RCVE ---");
 }
 
@@ -41,7 +27,13 @@ void serialEvent() //Called when data is available. Use Serial.read() to capture
   {      
       inChar = (char)Serial.read();
       servoCmd += inChar;
-      Serial.println("servoCmd = "+servoCmd);
+      //Serial.println("servoCmd = "+servoCmd);
+      if (servoCmd == "R\n")
+      {
+        Serial.println(String(rightServo.read()) + " " + String(leftServo.read()));
+        servoCmd = "";
+        inChar = "";
+      }
       if (inChar == ' ')
       {
         rightServo.write(servoCmd.toInt());
