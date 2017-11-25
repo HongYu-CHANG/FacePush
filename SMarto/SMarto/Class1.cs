174998678d7621b5357baff8cf3d272fe12d2946
RedFishChang <harris32916@gmail.com>
2017-11-25 13:31:52 +0800

    chang framework

--- SMarto/SMarto/Class1.cs
+++ SMarto/SMarto/Class1.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Ports;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace SMarto
 {
