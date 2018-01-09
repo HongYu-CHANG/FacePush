764b2ef06eca0cda55c23bdc0e6edf8ce0860de7
RedFishChang <harris32916@gmail.com>
2018-01-09 13:54:44 +0800

    add 2 applications

---
+++ DC MOTOR/DCmotor_Applicaton/Hurricane Wind FX/Assets/Hurricane_Wind_FX/Library/_Scripts/OSCReciever.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniOSC;
+using OSCsharp.Data;
+
+public class OSCReciever :  UniOSCEventTarget {
+
+    public GameObject controlObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnOSCMessageReceived(UniOSCEventArgs args)
+    {
+        OscMessage msg = (OscMessage)args.Packet;
+        if (msg.Data.Count < 1) return;
+
+        int scale = (int)msg.Data[0];
+        controlObject.transform.localScale = new Vector3((float)scale / 1023.0f, (float)scale / 1023.0f, (float)scale / 1023.0f);
+
+    }
+}
---
+++ DC MOTOR/DCmotor_Applicaton/Hurricane Wind FX/Assets/Hurricane_X_Motor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hurricane_X_Motor : MonoBehaviour {
+
+	public ParticleSystem Debris;
+	public ParticleSystem Tumbleweed;
+	public ParticleSystem Wind_Particles;
+	public ParticleSystem Leaf;
+	public OSCSender R_OSCSender;
+    public OSCSender L_OSCSender;
+    //private OSCSender ROSCSender;
+    //private OSCSender LOSCSender;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//ROSCSender = RMotor.GetComponent<OSCSender>();
+    	R_OSCSender.setWhichMotor("R");
+    	//LOSCSender = LMotor.GetComponent<OSCSender>();
+    	L_OSCSender.setWhichMotor("L");	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
