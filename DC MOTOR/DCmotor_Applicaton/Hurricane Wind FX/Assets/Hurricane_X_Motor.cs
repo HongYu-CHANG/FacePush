dad2f5dd826641aa18c42379a38edebc6bebf17d
RedFishChang <harris32916@gmail.com>
2018-01-09 14:47:15 +0800

    can control hurricane speed

--- DC MOTOR/DCmotor_Applicaton/Hurricane Wind FX/Assets/Hurricane_X_Motor.cs
+++ DC MOTOR/DCmotor_Applicaton/Hurricane Wind FX/Assets/Hurricane_X_Motor.cs
@@ -10,6 +10,10 @@ public class Hurricane_X_Motor : MonoBehaviour {
 	public ParticleSystem Leaf;
 	public OSCSender R_OSCSender;
     public OSCSender L_OSCSender;
+    public int ConvertTime = 3;
+    private bool HurricaneWork = false;
+    private float timer_f = 0f;
+    private int timer_i = 0;
     //private OSCSender ROSCSender;
     //private OSCSender LOSCSender;
 
@@ -19,12 +23,33 @@ public class Hurricane_X_Motor : MonoBehaviour {
 		//ROSCSender = RMotor.GetComponent<OSCSender>();
     	R_OSCSender.setWhichMotor("R");
     	//LOSCSender = LMotor.GetComponent<OSCSender>();
-    	L_OSCSender.setWhichMotor("L");	
+    	L_OSCSender.setWhichMotor("L");
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		timer_f += Time.deltaTime;
+		timer_i = (int) timer_f;
+		if(timer_i % ConvertTime == 0 && !HurricaneWork)
+		{
+			HurricaneWork = true;
+			Debug.Log(timer_i);
+			Random.seed = System.Guid.NewGuid().GetHashCode();
+			int speed = Random.Range(1, 11);
+		    Debug.Log("Speed = " + speed);
+	        var Debrismain = Debris.main;
+	 		Debrismain.simulationSpeed = speed;
+	 		var Tumbleweedmain = Tumbleweed.main;
+	 		Tumbleweedmain.simulationSpeed = speed;
+	 		var Wind_Particlesmain = Wind_Particles.main;
+	 		Wind_Particlesmain.simulationSpeed = speed;
+	 		var Leafmain = Leaf.main;
+	 		Leafmain.simulationSpeed = speed;
+		}
+		else if (timer_i % ConvertTime != 0 && HurricaneWork)
+		{
+			HurricaneWork = false;
+		}
 	}
 }
