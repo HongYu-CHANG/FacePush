83f91c0842df5c36d7b76f35543f1f4f061047d2
RedFishChang <harris32916@gmail.com>
2018-01-18 15:30:50 +0800

    wind with view guide

--- DC MOTOR/DCmotor_Applicaton/Hurricane Wind FX/Assets/Hurricane_Wind_FX/Scripts/Hurricane_X_Motor.cs
+++ DC MOTOR/DCmotor_Applicaton/Hurricane Wind FX/Assets/Hurricane_Wind_FX/Scripts/Hurricane_X_Motor.cs
@@ -13,12 +13,15 @@ public class Hurricane_X_Motor : MonoBehaviour {
 	public GameObject CameraEye;
 	public List<OSCSender> OSCSenderS;
     public int ConvertTime;
+    public int StopTime;
     private bool HurricaneWork = false;
+    private bool HurricaneStop = false;
     private float timer_f = 0f;
     private int timer_i = 0;
     private float rotationY;
+    private float initialY;
 	private int motorActive;
-	private int pastSpeed;
+	private int lastSpeed = 5;
     private enum Motors
     {
         Left = 0,
@@ -30,46 +33,64 @@ public class Hurricane_X_Motor : MonoBehaviour {
 	{
     	OSCSenderS[(int)Motors.Left].setWhichMotor("L");
     	OSCSenderS[(int)Motors.Right].setWhichMotor("R");
-		pastSpeed = 5;
+    	initialY = CameraEye.transform.rotation.eulerAngles.y;
     }
 	void Update () 
 	{
 		rotationY = CameraEye.transform.rotation.eulerAngles.y;
+		
 		timer_f += Time.deltaTime;
 		timer_i = (int) timer_f;
 		
 		if (rotationY >= 215 && rotationY <= 340)//R
 		{
+			//motorActive = (int) Motors.Both;
 			motorActive = (int) Motors.Right;
 		}
 		else if (rotationY >= 50 && rotationY <= 175)//L
 		{
+			//motorActive = (int) Motors.Both;
 			motorActive = (int) Motors.Left;
 		}
 		else
 		{
 			 motorActive = (int) Motors.Both;
 		}
-        if (timer_i % ConvertTime == 0 && !HurricaneWork)
+
+		if((timer_i - ConvertTime) % (ConvertTime+StopTime) == 0 && !HurricaneStop)
 		{
+			HurricaneStop = true;
+			Debug.Log("Time = " + timer_i);
+			Wind_Audio.volume = 0;
+			var tempMain = Debris.main;
+	 		tempMain.simulationSpeed = 0;
+	 		tempMain = Tumbleweed.main;
+	 		tempMain.simulationSpeed = 0;
+	 		tempMain = Wind_Particles.main;
+	 		tempMain.simulationSpeed = 0;
+	 		tempMain = Leaf.main;
+	 		tempMain.simulationSpeed = 0;
+			motorStop();
+		}
+		else if ((timer_i - ConvertTime) % (ConvertTime+StopTime) != 0 && HurricaneStop)
+			HurricaneStop = false;
+
+        if (timer_i % (ConvertTime+StopTime) == 0 && !HurricaneWork)
+		{
+			
 			HurricaneWork = true;
 			Debug.Log("Time = " + timer_i);
-            Debug.Log("pastSpeed = " + pastSpeed);
 			
 			int speed = UnityEngine.Random.Range(1, 11);
-			
-			while(ABS(pastSpeed - speed) > 3 || pastSpeed == speed)
+			while(ABS(lastSpeed - speed) > 3 || lastSpeed == speed)
 			{
-				Debug.Log("ABS = " + ABS(pastSpeed - speed));
-				Debug.Log("Loop speed = " + speed);
 				speed = UnityEngine.Random.Range(1, 11);
 			}
-            pastSpeed = speed;
+            lastSpeed = speed;
 		    Debug.Log("Speed = " + speed);
 
 		    //audio source
 		    Wind_Audio.volume = remapping((float)speed, 1f, 10f, 0.1f, 1f);
-		    Debug.Log("Volume = " + Wind_Audio.volume);
 
 		    //particle system setting
 	        var tempMain = Debris.main;
@@ -84,11 +105,10 @@ public class Hurricane_X_Motor : MonoBehaviour {
             // DC motor
             int Speed = (int)remapping((float)speed, 1f, 10f, 85f, 255f);
 			Debug.Log("Motor = " + Speed);
+			Debug.Log("Dir = " + (int)remapping((float)Speed, 85f, 255f, 5, 30));
 			StartCoroutine(SHAKEWORK(Speed));
-			
-				
 		}
-		else if (timer_i % ConvertTime != 0 && HurricaneWork)
+		else if (timer_i % (ConvertTime+StopTime) != 0 && HurricaneWork)
 			HurricaneWork = false;
 	}
 
@@ -108,37 +128,74 @@ public class Hurricane_X_Motor : MonoBehaviour {
 
 	IEnumerator SHAKEWORK(int tempSpeed)
 	{
+		
 		float tempTime = UnityEngine.Random.Range(0.01f, 0.03f);
 		int FirstMotor = 0, SecondMotor = 0;
-		
-		for(int i = 0 ; i < ConvertTime; i++)
+		int turnDirFirst = 1, turnDirSecond = 0;
+		Quaternion quate = Quaternion.identity;
+		Vector3 temp = new Vector3(0, initialY + (int)remapping((float)tempSpeed, 85f, 255f, 5, 35) * (-1), 0);
+		List<Vector3> turnDir = new List<Vector3>();
+		turnDir.Add(temp);
+		temp = new Vector3(0, initialY + (int)remapping((float)tempSpeed, 85f, 255f, 5, 35), 0);
+		turnDir.Add(temp);
+		if(motorActive == (int)Motors.Both)
 		{
+			 motorPressure( tempSpeed );
+		}
+		else
+		{
+			FirstMotor = motorActive;
+			SecondMotor = FirstMotor == (int)Motors.Left ? (int)Motors.Right : (int)Motors.Left;
+			OSCSenderS[FirstMotor].SendOSCMessageTriggerMethod("RELEASE", 0, 1);
+			OSCSenderS[SecondMotor].SendOSCMessageTriggerMethod("FORWARD", tempSpeed, 1);
+		}
+		
+		for(int i = 0 ; i < (ConvertTime - 1); i++)
+		{
+	       	
 	       	if(motorActive == (int)Motors.Both)
 			{
 				FirstMotor = UnityEngine.Random.Range(0, 2);
 				SecondMotor = FirstMotor == (int)Motors.Left ? (int)Motors.Right : (int)Motors.Left;
-				OSCSenderS[FirstMotor].SendOSCMessageTriggerMethod("FORWARD", tempSpeed , 1);//加壓
-				yield return new WaitForSeconds(0.35f - tempTime);
-				OSCSenderS[SecondMotor].SendOSCMessageTriggerMethod("FORWARD", tempSpeed, 1);
-				yield return new WaitForSeconds(0.35f + tempTime);
+				turnDirFirst = FirstMotor == (int)Motors.Left ? -1 : 1;
+				turnDirSecond = turnDirFirst * (-1);
 				OSCSenderS[FirstMotor].SendOSCMessageTriggerMethod("BACKWARD", tempSpeed, 1);//放鬆
-				yield return new WaitForSeconds(0.15f - tempTime);
+				yield return new WaitForSeconds(0.2f - tempTime);
+
 				OSCSenderS[SecondMotor].SendOSCMessageTriggerMethod("BACKWARD", tempSpeed, 1);
-				yield return new WaitForSeconds(0.15f + tempTime);
+				yield return new WaitForSeconds(0.2f + tempTime);
+
+				Debug.Log("turnDir = " + turnDir[FirstMotor]);
+				Debug.Log(CameraEye.transform.rotation.eulerAngles);
+				OSCSenderS[FirstMotor].SendOSCMessageTriggerMethod("FORWARD", tempSpeed , 1);//加壓
+				quate.eulerAngles = turnDir[FirstMotor];
+				Debug.Log(CameraEye.transform.rotation.eulerAngles);
+				CameraEye.transform.rotation = quate;
+				yield return new WaitForSeconds(0.2f - tempTime);
+				Debug.Log("turnDir = " + turnDir[SecondMotor]);
+				Debug.Log(CameraEye.transform.rotation.eulerAngles);
+				OSCSenderS[SecondMotor].SendOSCMessageTriggerMethod("FORWARD", tempSpeed, 1);
+				quate.eulerAngles = turnDir[SecondMotor];
+				Debug.Log(CameraEye.transform.rotation.eulerAngles);
+				CameraEye.transform.rotation = quate;
+				yield return new WaitForSeconds(0.2f + tempTime);
+				
+				
 			}
 			else 
 			{
 				FirstMotor = motorActive;
 				SecondMotor = FirstMotor == (int)Motors.Left ? (int)Motors.Right : (int)Motors.Left;
-				OSCSenderS[SecondMotor].SendOSCMessageTriggerMethod("FORWARD", tempSpeed, 1);
-				yield return new WaitForSeconds(0.25f + tempTime);
 				OSCSenderS[FirstMotor].SendOSCMessageTriggerMethod("RELEASE", 0, 1);
+
 				OSCSenderS[SecondMotor].SendOSCMessageTriggerMethod("BACKWARD", tempSpeed, 1);
-				yield return new WaitForSeconds(0.25f - tempTime);
+				yield return new WaitForSeconds(0.2f - tempTime);
 				OSCSenderS[SecondMotor].SendOSCMessageTriggerMethod("FORWARD", tempSpeed, 1);
-				yield return new WaitForSeconds(0.35f + tempTime);
+				yield return new WaitForSeconds(0.2f + tempTime);
 				OSCSenderS[SecondMotor].SendOSCMessageTriggerMethod("BACKWARD", tempSpeed, 1);
-				yield return new WaitForSeconds(0.25f - tempTime);
+				yield return new WaitForSeconds(0.2f - tempTime);
+				OSCSenderS[SecondMotor].SendOSCMessageTriggerMethod("FORWARD", tempSpeed, 1);
+				yield return new WaitForSeconds(0.2f + tempTime);
 			}
     	}	
 	}
