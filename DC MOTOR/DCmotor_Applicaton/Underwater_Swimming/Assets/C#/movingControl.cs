04f410fc6de77fea9f3eef93db0a1a184e70fc8b
aa48330128 <aa48330128.cs05g@nctu.edu.tw>
2018-02-01 00:03:25 +0800

    Add swimming movements: swimmer's rotation & direction

--- DC MOTOR/DCmotor_Applicaton/Underwater_Swimming/Assets/C#/movingControl.cs
+++ DC MOTOR/DCmotor_Applicaton/Underwater_Swimming/Assets/C#/movingControl.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class movingControl : MonoBehaviour {
 
+    public GameObject headpos;
+    public GameObject bodypos;
+
+    private Vector3 body_head_direction;
+
+    float halfBodyLength = 0;//1.5f;
+    float rotateDegree = 0f;
+
     // Use this for initialization
     void Start () {
-
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y, transform.position.z);
-        Debug.Log("=== move: " + transform.position.ToString("F4") + " ===");
+        body_head_direction = headpos.transform.position - bodypos.transform.position;
+        transform.position = transform.position + body_head_direction * (0.01f);
 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.Rotate(Vector3.down);
-        }
+        //transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y, transform.position.z);
+        //Debug.Log("=== move: " + transform.position.ToString("F4") + " ===");
 
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            transform.Rotate(Vector3.up);
-        }
+        //get the degree from directionControl.cs
+        //rotateDegree = ???;
+
+        //transform.position = new Vector3(transform.position.x - halfBodyLength * Mathf.Sin(rotateDegree * Mathf.Deg2Rad) , transform.position.y, transform.position.z + halfBodyLength * Mathf.Cos(rotateDegree * Mathf.Deg2Rad));
+
     }
 }
