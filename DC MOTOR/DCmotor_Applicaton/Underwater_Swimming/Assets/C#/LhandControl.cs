e2d190605a1cb210f6ec00e90d9cd6a4a224630c
aa48330128 <aa48330128.cs05g@nctu.edu.tw>
2018-02-01 14:32:39 +0800

    move swimmer's hands

---
+++ DC MOTOR/DCmotor_Applicaton/Underwater_Swimming/Assets/C#/LhandControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LhandControl : MonoBehaviour {
+
+    public GameObject LhandMoveObj;
+    private Vector3 LlastPos;
+
+	// Use this for initialization
+	void Start () {
+
+        LlastPos = LhandMoveObj.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(LhandMoveObj.transform.position != LlastPos)
+        {
+            transform.Rotate((Vector3.down) * (LhandMoveObj.transform.position.z - LlastPos.z));
+            Debug.Log("lastPos = " + LlastPos);
+            Debug.Log("nowPos = " + transform.position);
+            LlastPos = LhandMoveObj.transform.position;
+        }
+        else
+        {
+            Debug.Log("same L pos");
+        }
+
+	}
+
+}
