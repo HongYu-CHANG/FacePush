10a6294589a8c19661aac1cedaf69b499616ca73
aa48330128 <aa48330128.cs05g@nctu.edu.tw>
2018-02-06 23:10:39 +0800

    show trackers' pos(spheres), emptyBody ok

--- DC MOTOR/DCmotor_Applicaton/Underwater_Swimming/Assets/C#/handDisplayControl.cs
+++ DC MOTOR/DCmotor_Applicaton/Underwater_Swimming/Assets/C#/handDisplayControl.cs
@@ -7,6 +7,9 @@ public class handDisplayControl : MonoBehaviour {
     public GameObject tracker;
     private Vector3 trackerLastPos;
 
+	public int frameSegment;
+	private int counter = 0;
+
 	// Use this for initialization
 	void Start () {
         trackerLastPos = tracker.transform.position;
@@ -14,13 +17,25 @@ public class handDisplayControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
+	/*
     void FixedUpdate()
     {
-        transform.position = transform.position + (tracker.transform.position - trackerLastPos) ;
-        trackerLastPos = tracker.transform.position;
+		if(counter < frameSegment - 1)
+		{
+			counter++;
+		}
+		else
+		{
+			transform.position = transform.position + (tracker.transform.position - trackerLastPos) * 0.1f ;
+			//transform.position = (tracker.transform.position - trackerLastPos
+			trackerLastPos = tracker.transform.position;
+			counter = 0;
+		}
+		
     }
+	*/
 
 }
