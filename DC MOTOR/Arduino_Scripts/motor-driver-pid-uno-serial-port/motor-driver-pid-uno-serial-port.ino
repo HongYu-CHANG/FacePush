f2fa698a7fa307c1837c50aeb7ff6e7bb1f5db41
RedFishCHANG <redfishchang.cs06g@nctu.edu.tw>
2018-06-19 19:33:35 +0800

    Redfactor arduino code

--- DC MOTOR/Arduino_Scripts/motor-driver-pid-uno-serial-port/motor-driver-pid-uno-serial-port.ino
+++ DC MOTOR/Arduino_Scripts/motor-driver-pid-uno-serial-port/motor-driver-pid-uno-serial-port.ino
@@ -1,285 +1,248 @@
-/* This sketch supports NILab Project FacePush, as described below:
- * - UNO receives message directly from PC by serial port.
- * - uses Arduino UNO and Monster Motor Shield VNH2SP30 to control 2 DC motors.
- * - control DC motors by rotary encoders and PID
- * - use interrupt pins D2, D3, D10, and D11 (by PinChangeInterrupt library)
- *   to track the value of rotary encoders.
- * 
- *   23.02.18 wjtseng93
- */
+
+#include <PinChangeInt.h>
+#include <PID_v1.h>
 #define SERIAL_BAUD 9600
-//====================================================================================
-// Monster Motor Driver VNH2SP30
+#define DEBUG 0 
+
+//All Ｍotor Parameter
 #define BRAKE 0
 #define CW    1
 #define CCW   2
-#define CS_THRESHOLD 15   // Definition of safety current (Check: "1.3 Monster Shield Example").
-
-//MOTOR 1
-#define MOTOR_A1_PIN 7
-#define MOTOR_B1_PIN 8
-
-//MOTOR 2
-#define MOTOR_A2_PIN 4
-#define MOTOR_B2_PIN 9
-
-#define PWM_MOTOR_1 5
-#define PWM_MOTOR_2 6
-
-#define CURRENT_SEN_1 A2
-#define CURRENT_SEN_2 A3
-
-#define EN_PIN_1 A0
-#define EN_PIN_2 A1
-
-#define MOTOR_1 0
-#define MOTOR_2 1
-
-//default motor speed and motor state
-short speedLeft = 255;
-short speedRight = 255;
-unsigned short usMotor_Status = BRAKE;
-int motorParameters[4]; // angle left, speed left, angle right, speed right
-
-//====================================================================================
-// Interrupt and Rotary encoder
-// Arduino UNO has 2 interrupt pins, D2, D3
-// use PinChangeInt to make D10, D11 into interrupt pins
-#include <PinChangeInt.h>
-int encoderLeftPin1 = 3; // interrupt 0
-int encoderLeftPin2 = 2; // interrupt 1
-int encoderRightPin1 = 10; // use PinChangeInt make it into interrupt pin
-int encoderRightPin2 = 11;
-
-// rotary encoders
-volatile long leftLastEncoded = 0;
-volatile long rightLastEncoded = 0;
-
-volatile long encoderLeftValue = 0;
-volatile long encoderRightValue = 0;
-
-//====================================================================================
-// PID motor control
-#include <PID_v1.h>
+int AllMotor_Parameters[4]; // angle left, speed left, angle right, speed right
+//PID Parameter
 double kp = 0.75, ki = 0.09, kd = 0.01;
-// input: current position (value of rotary encoder)
-// output: result (where to go)
-// setPoint: target position (position cmd from Feather)
-double inputLeft = 0, outputLeft = 0, setPointLeft = 0;
-double inputRight = 0, outputRight = 0, setPointRight = 0;
-// DIRECT was defined in PID_v1.h
-PID leftPID(&inputLeft, &outputLeft, &setPointLeft, kp, ki, kd, DIRECT); 
-PID rightPID(&inputRight, &outputRight, &setPointRight, kp, ki, kd, DIRECT);
+//Read String Parameter
+String ReadString_Input = "";
+bool ReadString_Complete = false;
+int MotorTimer = 0;
 
-String inputString = "";
-bool stringComplete = false;
-int timer = 0;
+// Left default setting(Motor，Rotary Encoder，PID)
+#define LeftMotor_A1_PIN 7
+#define LeftMotor_B1_PIN 8
+#define LeftMotor_PWM 5
+#define LeftMotor_EnablePin A0
+#define LeftMotor 1
+short LeftMotor_Speed = 255;
+int LeftEncoder_Pin1 = 3; // interrupt 0
+int LeftEncoder_Pin2 = 2; // interrupt 1
+volatile long LeftEncoder_LastValue = 0;
+volatile long LeftEncoder_Value = 0;
+double LeftPID_Input = 0;// input: current position (value of rotary encoder)
+double LeftPID_Output = 0;// output: result (where to go)
+double LeftPID_Target = 0;// Target: target position (position cmd from Feather)
+PID LeftPID_Contorller(&LeftPID_Input, &LeftPID_Output, &LeftPID_Target, kp, ki, kd, DIRECT); 
+
+//Right default setting(Motor，Rotary Encoder，PID)
+#define RightMotor_A2_PIN 4
+#define RightMotor_B2_PIN 9
+#define RightMotor_PWM 6
+#define RightMotor_EnablePin A1
+#define RightMotor 2
+short RightMotor_Speed = 255;
+int RightEncoder_Pin1 = 10;//use PinChangeInt to simulate Interript
+int RightEncoder_Pin2 = 11;//use PinChangeInt to simulate Interript
+volatile long RightEncoder_LastValue = 0;
+volatile long RightEncoder_Value = 0;
+double RightPID_Input = 0;// input: current position (value of rotary encoder)
+double RightPID_Output = 0;// output: result (where to go)
+double RightPID_Target = 0;// Target: target position (position cmd from Feather)
+PID RightPID_Contorller(&RightPID_Input, &RightPID_Output, &RightPID_Target, kp, ki, kd, DIRECT);
+
 void setup()                         
 {
   Serial.begin(SERIAL_BAUD);
-
-  // DC motor setup
-  pinMode(MOTOR_A1_PIN, OUTPUT);
-  pinMode(MOTOR_B1_PIN, OUTPUT);
-
-  pinMode(MOTOR_A2_PIN, OUTPUT);
-  pinMode(MOTOR_B2_PIN, OUTPUT);
-
-  pinMode(PWM_MOTOR_1, OUTPUT);
-  pinMode(PWM_MOTOR_2, OUTPUT);
-
-  pinMode(CURRENT_SEN_1, OUTPUT);
-  pinMode(CURRENT_SEN_2, OUTPUT);  
-
-  pinMode(EN_PIN_1, OUTPUT);
-  pinMode(EN_PIN_2, OUTPUT);
-
-  // rotary encoder setup
-  pinMode(encoderLeftPin1, INPUT); 
-  pinMode(encoderLeftPin2, INPUT);
-  pinMode(encoderRightPin1, INPUT); 
-  pinMode(encoderRightPin2, INPUT);
-
-  digitalWrite(encoderLeftPin1, HIGH); //turn pullup resistor on
-  digitalWrite(encoderLeftPin2, HIGH); //turn pullup resistor on
-  digitalWrite(encoderRightPin1, HIGH); //turn pullup resistor on
-  digitalWrite(encoderRightPin2, HIGH); //turn pullup resistor on
-
-  //call updateEncoder() when any high/low changed seen
-  attachInterrupt(digitalPinToInterrupt(encoderLeftPin1), updateLeftEncoder, CHANGE); 
-  attachInterrupt(digitalPinToInterrupt(encoderLeftPin2), updateLeftEncoder, CHANGE);
-  attachPinChangeInterrupt(encoderRightPin1, updateRightEncoder, CHANGE); 
-  attachPinChangeInterrupt(encoderRightPin2, updateRightEncoder, CHANGE);
-
-  // PID control setup
-  leftPID.SetMode(AUTOMATIC);
-  rightPID.SetMode(AUTOMATIC);
-  leftPID.SetOutputLimits(-speedLeft, speedLeft);
-  rightPID.SetOutputLimits(-speedRight, speedRight);
+  LeftSetup();
+  RightSetup();
 }
 
 void loop() 
 {
-//  Uncomment below to check values of PID  
-//  Serial.print(inputLeft); Serial.print(" ");
-//  Serial.print(setPointLeft); Serial.print(" ");
-//  Serial.print(outputLeft); Serial.print(" ");
-//  Serial.print(inputRight); Serial.print(" ");
-//  Serial.print(setPointRight); Serial.print(" ");
-//  Serial.println(outputRight); Serial.print(" ");
-
-  inputLeft = encoderLeftValue;
-  inputRight = encoderRightValue;
-  // Update PID control
-//  int currentTime = millis();
-
-  motorPIDControl(&encoderLeftValue, &setPointLeft, &outputLeft, &leftPID, EN_PIN_1, MOTOR_1);
-  motorPIDControl(&encoderRightValue, &setPointRight, &outputRight, &rightPID, EN_PIN_2, MOTOR_2);
-
-//  if (inputRight == setPointRight && inputLeft == setPointLeft) {
-//    timer = 0;
-//  }
-//  else {
-//    timer += millis() - currentTime;
-//  }
-//  if (timer > 90) {
-//    setPointLeft = inputLeft;
-//    outputLeft = 0;
-//    setPointRight = inputRight;
-//    outputRight = 0;
-//  }
-
-
+//  Uncomment below to check values of PID
+#if DEBUG == 1  
+  Serial.print(LeftPID_Input); Serial.print(" ");
+  Serial.print(LeftPID_Target); Serial.print(" ");
+  Serial.print(LeftPID_Output); Serial.print(" ");
+  Serial.print(RightPID_Input); Serial.print(" ");
+  Serial.print(RightPID_Target); Serial.print(" ");
+  Serial.print(RightPID_Output); Serial.print(" ");
+  Serial.print(MotorTimer); Serial.println(" ");
+#else
+  delay(40);
+  //Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
+#endif
   
-//  Uncomment below to send data in serial port
-  while(Serial.available())
+
+  LeftPID_Input = LeftEncoder_Value;
+  motorPIDControl(&LeftEncoder_Value, &LeftPID_Target, &LeftPID_Output, &LeftPID_Contorller, LeftMotor_EnablePin, LeftMotor);
+  RightPID_Input = RightEncoder_Value;
+  motorPIDControl(&RightEncoder_Value, &RightPID_Target, &RightPID_Output, &RightPID_Contorller, RightMotor_EnablePin, RightMotor);
+
+  if (RightPID_Input != RightPID_Target && LeftPID_Input != LeftPID_Target) MotorTimer++;
+  if (MotorTimer > 45) 
   {
-    digitalWrite(EN_PIN_1, HIGH);
-    digitalWrite(EN_PIN_2, HIGH); 
-    char c = Serial.read();
-    inputString += c;
-    if (c == '\n') {
-      stringComplete = true;   
-    }
+    digitalWrite(LeftMotor_EnablePin, LOW);
+    digitalWrite(RightMotor_EnablePin, LOW);
   }
-  if (stringComplete) {
-//    Serial.println(inputString);
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < inputString.length(); j++) {
-        if (inputString.substring(j, j + 1) == " ")
+  
+  while(Serial.available())//Read Data
+  {
+    digitalWrite(LeftMotor_EnablePin, HIGH);
+    digitalWrite(RightMotor_EnablePin, HIGH); 
+    MotorTimer = 0;
+    char c = Serial.read();
+    ReadString_Input += c;
+    if (c == '\n')ReadString_Complete = true;   
+  }
+  if (ReadString_Complete) 
+  {
+    for (int i = 0; i < 4; i++) 
+    {
+      for (int j = 0; j < ReadString_Input.length(); j++) 
+      {
+        if (ReadString_Input.substring(j, j + 1) == " " || ReadString_Input.substring(j, j + 1) == "\n")
         {
-          motorParameters[i] = inputString.substring(0, j).toInt();
-          if (i == 2) {
-            motorParameters[i + 1] = inputString.substring(j + 1).toInt();
-          }
-          else {
-            inputString = inputString.substring(j + 1);
-          }
+          AllMotor_Parameters[i] = ReadString_Input.substring(0, j).toInt();
+          ReadString_Input = ReadString_Input.substring(j + 1);
           break;
 	      }
       }
     }
-    setPointLeft = (double) motorParameters[0];
-    speedLeft = (short) motorParameters[1];
-    setPointRight = (double) motorParameters[2];
-    speedRight = (short) motorParameters[3];
-    leftPID.SetOutputLimits(-speedLeft, speedLeft);
-    rightPID.SetOutputLimits(-speedRight, speedRight);
-//    for (int i = 0; i < 4; i++) {
-//      Serial.println(motorParameters[i]);
-//    }
-    stringComplete = false;
-    inputString = "";
+    LeftPID_Target = (double) AllMotor_Parameters[0];
+    LeftMotor_Speed = (short) AllMotor_Parameters[1];
+    LeftPID_Contorller.SetOutputLimits(-LeftMotor_Speed, LeftMotor_Speed);
+    
+    RightPID_Target = (double) AllMotor_Parameters[2];
+    RightMotor_Speed = (short) AllMotor_Parameters[3];
+    RightPID_Contorller.SetOutputLimits(-RightMotor_Speed, RightMotor_Speed);
+    ReadString_Complete = false;
+    ReadString_Input = "";
   } 
 }
 
-
-
-// Function that controls the variables:
-// motor(0 or 1), direction (cw or ccw), and pwm (0 to 255)
 void motorGo(uint8_t motor, uint8_t direct, uint8_t pwm) 
 {
-  if(motor == MOTOR_1)
+  if(motor == LeftMotor)
   {
     if(direct == CW)
     {
-      digitalWrite(MOTOR_A1_PIN, LOW); 
-      digitalWrite(MOTOR_B1_PIN, HIGH);
+      digitalWrite(LeftMotor_A1_PIN, LOW); 
+      digitalWrite(LeftMotor_B1_PIN, HIGH);
     }
     else if(direct == CCW)
     {
-      digitalWrite(MOTOR_A1_PIN, HIGH);
-      digitalWrite(MOTOR_B1_PIN, LOW);      
+      digitalWrite(LeftMotor_A1_PIN, HIGH);
+      digitalWrite(LeftMotor_B1_PIN, LOW);      
     }
     else
     {
-      digitalWrite(MOTOR_A1_PIN, LOW);
-      digitalWrite(MOTOR_B1_PIN, LOW);            
+      digitalWrite(LeftMotor_A1_PIN, LOW);
+      digitalWrite(LeftMotor_B1_PIN, LOW);            
     }
     
-    analogWrite(PWM_MOTOR_1, pwm); 
+    analogWrite(LeftMotor_PWM, pwm); 
   }
-  else if(motor == MOTOR_2)
+  else if(motor == RightMotor)
   {
     if(direct == CW)
     {
       // CW input for right motor is actually CCW
-      digitalWrite(MOTOR_A2_PIN, HIGH);
-      digitalWrite(MOTOR_B2_PIN, LOW);
+      digitalWrite(RightMotor_A2_PIN, HIGH);
+      digitalWrite(RightMotor_B2_PIN, LOW);
     }
     else if(direct == CCW)
     {
       // CCW input for right motor is actually CW
-      digitalWrite(MOTOR_A2_PIN, LOW);
-      digitalWrite(MOTOR_B2_PIN, HIGH);      
+      digitalWrite(RightMotor_A2_PIN, LOW);
+      digitalWrite(RightMotor_B2_PIN, HIGH);      
     }
     else
     {
-      digitalWrite(MOTOR_A2_PIN, LOW);
-      digitalWrite(MOTOR_B2_PIN, LOW);            
+      digitalWrite(RightMotor_A2_PIN, LOW);
+      digitalWrite(RightMotor_B2_PIN, LOW);            
     }
     
-    analogWrite(PWM_MOTOR_2, pwm);
+    analogWrite(RightMotor_PWM, pwm);
   }
 }
 
-void updateLeftEncoder() {
-  long MSB = digitalRead(encoderLeftPin1); //MSB = most significant bit
-  long LSB = digitalRead(encoderLeftPin2); //LSB = least significant bit
-
-  long encoded = (MSB << 1) | LSB; //converting the 2 pin value to single number
-  long sum  = (leftLastEncoded << 2) | encoded; //adding it to the previous encoded value
-
-  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderLeftValue ++;
-  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderLeftValue --;
-
-  leftLastEncoded = encoded; //store this value for next time
-}
-
-void updateRightEncoder() {
-  long MSB = digitalRead(encoderRightPin1); //MSB = most significant bit
-  long LSB = digitalRead(encoderRightPin2); //LSB = least significant bit
-
-  long encoded = (MSB << 1) | LSB; //converting the 2 pin value to single number
-  long sum  = (rightLastEncoded << 2) | encoded; //adding it to the previous encoded value
-
-  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderRightValue ++;
-  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderRightValue --;
-
-  rightLastEncoded = encoded; //store this value for next time
-}
-
-void motorPIDControl(volatile long *encoderValue, double *setPoint, double *output, PID *motorPID, uint8_t EN_PIN, uint8_t MOTOR) {
+void motorPIDControl(volatile long *encoderValue, double *setPoint, double *output, PID *motorPID, uint8_t EN_PIN, uint8_t Motor) 
+{
     motorPID->Compute();
 
-    if (*output > 0) {
-      motorGo(MOTOR, CW, *output);
+    if (*output > 0) 
+    {
+      motorGo(Motor, CW, *output);
     }
-    else {
-      motorGo(MOTOR, CCW, abs(*output));
+    else if (*output < 0)
+    {
+      motorGo(Motor, CCW, abs(*output));
     }
     if (*encoderValue == *setPoint) {
-      motorGo(MOTOR, BRAKE, 0);
+      
+      motorGo(Motor, BRAKE, 0);
       *output = 0;
     }
+}
+
+void LeftSetup()
+{
+  // Left setup (Motor，Rotary Encoder，PID)
+  pinMode(LeftMotor_A1_PIN, OUTPUT);
+  pinMode(LeftMotor_B1_PIN, OUTPUT);
+  pinMode(LeftMotor_PWM, OUTPUT);
+  pinMode(LeftMotor_EnablePin, OUTPUT);
+  pinMode(LeftEncoder_Pin1, INPUT); 
+  pinMode(LeftEncoder_Pin2, INPUT);
+  digitalWrite(LeftEncoder_Pin1, HIGH); //turn pullup resistor on
+  digitalWrite(LeftEncoder_Pin2, HIGH); //turn pullup resistor on
+  attachInterrupt(digitalPinToInterrupt(LeftEncoder_Pin1), LeftEncoder_Update, CHANGE); 
+  attachInterrupt(digitalPinToInterrupt(LeftEncoder_Pin2), LeftEncoder_Update, CHANGE);
+  RightPID_Contorller.SetMode(AUTOMATIC);
+  RightPID_Contorller.SetOutputLimits(-RightMotor_Speed, RightMotor_Speed);
+}
+
+void LeftEncoder_Update() 
+{
+  long MSB = digitalRead(LeftEncoder_Pin1); //MSB = most significant bit
+  long LSB = digitalRead(LeftEncoder_Pin2); //LSB = least significant bit
+
+  long encoded = (MSB << 1) | LSB; //converting the 2 pin value to single number
+  long sum  = (LeftEncoder_LastValue << 2) | encoded; //adding it to the previous encoded value
+
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) LeftEncoder_Value ++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) LeftEncoder_Value --;
+
+  LeftEncoder_LastValue = encoded; //store this value for next time
+}
+
+
+void RightSetup()
+{
+  // Right setup (Motor，Rotary Encoder，PID)
+  pinMode(RightMotor_A2_PIN, OUTPUT);
+  pinMode(RightMotor_B2_PIN, OUTPUT);
+  pinMode(RightMotor_PWM, OUTPUT);
+  pinMode(RightMotor_EnablePin, OUTPUT);
+  pinMode(RightEncoder_Pin1, INPUT); 
+  pinMode(RightEncoder_Pin2, INPUT);
+  digitalWrite(RightEncoder_Pin1, HIGH); //turn pullup resistor on
+  digitalWrite(RightEncoder_Pin2, HIGH); //turn pullup resistor on
+  attachPinChangeInterrupt(RightEncoder_Pin1, RightEncoder_Update, CHANGE); 
+  attachPinChangeInterrupt(RightEncoder_Pin2, RightEncoder_Update, CHANGE);
+  LeftPID_Contorller.SetMode(AUTOMATIC);
+  LeftPID_Contorller.SetOutputLimits(-LeftMotor_Speed, LeftMotor_Speed);
+}
+
+void RightEncoder_Update() 
+{
+  long MSB = digitalRead(RightEncoder_Pin1); //MSB = most significant bit
+  long LSB = digitalRead(RightEncoder_Pin2); //LSB = least significant bit
+
+  long encoded = (MSB << 1) | LSB; //converting the 2 pin value to single number
+  long sum  = (RightEncoder_LastValue << 2) | encoded; //adding it to the previous encoded value
+
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) RightEncoder_Value ++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) RightEncoder_Value --;
+
+  RightEncoder_LastValue = encoded; //store this value for next time
 }
