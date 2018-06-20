64be0e64a84178a5ca11c60e13601869e6e42016
RedFishCHANG <redfishchang.cs06g@nctu.edu.tw>
2018-06-20 14:12:51 +0800

    Arduino code refactor done and Right Motor use new PID

--- DC MOTOR/Arduino_Scripts/motor-driver-pid-uno-serial-port/motor-driver-pid-uno-serial-port.ino
+++ DC MOTOR/Arduino_Scripts/motor-driver-pid-uno-serial-port/motor-driver-pid-uno-serial-port.ino
@@ -5,24 +5,18 @@
 #define DEBUG 0 
 
 //All Ｍotor Parameter
-#define BRAKE 0
-#define CW    1
-#define CCW   2
-int AllMotor_Parameters[4]; // angle left, speed left, angle right, speed right
-//PID Parameter
-double kp = 0.75, ki = 0.09, kd = 0.01;
+int AllMotor_Parameters[4] = {0}; // angle left, speed left, angle right, speed right
+int MotorCounter = 0;
 //Read String Parameter
 String ReadString_Input = "";
-bool ReadString_Complete = false;
-int MotorTimer = 0;
 
 // Left default setting(Motor，Rotary Encoder，PID)
-#define LeftMotor_A1_PIN 7
-#define LeftMotor_B1_PIN 8
+#define LeftMotor_A1_PIN 8
+#define LeftMotor_B1_PIN 7
 #define LeftMotor_PWM 5
 #define LeftMotor_EnablePin A0
 #define LeftMotor 1
-short LeftMotor_Speed = 255;
+double LeftMotor_Speed = 255;
 int LeftEncoder_Pin1 = 3; // interrupt 0
 int LeftEncoder_Pin2 = 2; // interrupt 1
 volatile long LeftEncoder_LastValue = 0;
@@ -30,7 +24,8 @@ volatile long LeftEncoder_Value = 0;
 double LeftPID_Input = 0;// input: current position (value of rotary encoder)
 double LeftPID_Output = 0;// output: result (where to go)
 double LeftPID_Target = 0;// Target: target position (position cmd from Feather)
-PID LeftPID_Contorller(&LeftPID_Input, &LeftPID_Output, &LeftPID_Target, kp, ki, kd, DIRECT); 
+double Left_kp = 0.8, Left_ki = 0.09, Left_kd = 0.01; //PID Parameter
+PID LeftPID_Contorller(&LeftPID_Input, &LeftPID_Output, &LeftPID_Target, Left_kp, Left_ki, Left_kd, DIRECT); 
 
 //Right default setting(Motor，Rotary Encoder，PID)
 #define RightMotor_A2_PIN 4
@@ -38,7 +33,7 @@ PID LeftPID_Contorller(&LeftPID_Input, &LeftPID_Output, &LeftPID_Target, kp, ki,
 #define RightMotor_PWM 6
 #define RightMotor_EnablePin A1
 #define RightMotor 2
-short RightMotor_Speed = 255;
+double RightMotor_Speed = 255;
 int RightEncoder_Pin1 = 10;//use PinChangeInt to simulate Interript
 int RightEncoder_Pin2 = 11;//use PinChangeInt to simulate Interript
 volatile long RightEncoder_LastValue = 0;
@@ -46,145 +41,103 @@ volatile long RightEncoder_Value = 0;
 double RightPID_Input = 0;// input: current position (value of rotary encoder)
 double RightPID_Output = 0;// output: result (where to go)
 double RightPID_Target = 0;// Target: target position (position cmd from Feather)
-PID RightPID_Contorller(&RightPID_Input, &RightPID_Output, &RightPID_Target, kp, ki, kd, DIRECT);
+double Right_kp = 0.8, Right_ki = 0.09, Right_kd = 0.1; //PID Parameter
+PID RightPID_Contorller(&RightPID_Input, &RightPID_Output, &RightPID_Target, Right_kp, Right_ki, Right_kd, DIRECT);
 
 void setup()                         
 {
   Serial.begin(SERIAL_BAUD);
-  LeftSetup();
-  RightSetup();
+  LeftInitialSetting();
+  RightInitialSetting();
 }
 
 void loop() 
 {
-//  Uncomment below to check values of PID
-#if DEBUG == 1  
-  Serial.print(LeftPID_Input); Serial.print(" ");
-  Serial.print(LeftPID_Target); Serial.print(" ");
-  Serial.print(LeftPID_Output); Serial.print(" ");
-  Serial.print(RightPID_Input); Serial.print(" ");
-  Serial.print(RightPID_Target); Serial.print(" ");
-  Serial.print(RightPID_Output); Serial.print(" ");
-  Serial.print(MotorTimer); Serial.println(" ");
-#else
-  delay(40);
-  //Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
-#endif
-  
-
+  //  DEBUG == 1 below to check values of PID
+  #if DEBUG == 1  
+    Serial.print(LeftPID_Input); Serial.print(" ");
+    Serial.print(LeftPID_Target); Serial.print(" ");
+    Serial.print(LeftPID_Output); Serial.print(" ");
+    Serial.print(RightPID_Input); Serial.print(" ");
+    Serial.print(RightPID_Target); Serial.print(" ");
+    Serial.print(RightPID_Output); Serial.print(" ");
+    Serial.print(AllMotor_Parameters[0]); Serial.print(" ");
+    Serial.print(AllMotor_Parameters[1]); Serial.print(" ");
+    Serial.print(AllMotor_Parameters[2]); Serial.print(" ");
+    Serial.print(AllMotor_Parameters[3]); Serial.print(" ");
+    Serial.print(MotorCounter); Serial.println(" ");
+  #else
+    delay(50);
+  #endif
+    
   LeftPID_Input = LeftEncoder_Value;
-  motorPIDControl(&LeftEncoder_Value, &LeftPID_Target, &LeftPID_Output, &LeftPID_Contorller, LeftMotor_EnablePin, LeftMotor);
+  PID_Calculation(&LeftPID_Output, &LeftPID_Contorller, LeftMotor);
   RightPID_Input = RightEncoder_Value;
-  motorPIDControl(&RightEncoder_Value, &RightPID_Target, &RightPID_Output, &RightPID_Contorller, RightMotor_EnablePin, RightMotor);
+  PID_Calculation(&RightPID_Output, &RightPID_Contorller, RightMotor);
 
-  if (RightPID_Input != RightPID_Target && LeftPID_Input != LeftPID_Target) MotorTimer++;
-  if (MotorTimer > 45) 
+  //Control Motor noise
+  if (RightPID_Input != RightPID_Target && LeftPID_Input != LeftPID_Target) MotorCounter++;
+  if (MotorCounter > 45) 
   {
     digitalWrite(LeftMotor_EnablePin, LOW);
     digitalWrite(RightMotor_EnablePin, LOW);
   }
   
-  while(Serial.available())//Read Data
+  //Read Data and Handle Data
+  int i = 0;
+  while(Serial.available())
   {
     digitalWrite(LeftMotor_EnablePin, HIGH);
     digitalWrite(RightMotor_EnablePin, HIGH); 
-    MotorTimer = 0;
+    MotorCounter = 0;
     char c = Serial.read();
-    ReadString_Input += c;
-    if (c == '\n')ReadString_Complete = true;   
-  }
-  if (ReadString_Complete) 
-  {
-    for (int i = 0; i < 4; i++) 
+    if (c == ' ' || c == '\n')
     {
-      for (int j = 0; j < ReadString_Input.length(); j++) 
-      {
-        if (ReadString_Input.substring(j, j + 1) == " " || ReadString_Input.substring(j, j + 1) == "\n")
-        {
-          AllMotor_Parameters[i] = ReadString_Input.substring(0, j).toInt();
-          ReadString_Input = ReadString_Input.substring(j + 1);
-          break;
-	      }
-      }
+      AllMotor_Parameters[i] = ReadString_Input.toInt();
+      i++;
+      ReadString_Input = "";
     }
-    LeftPID_Target = (double) AllMotor_Parameters[0];
-    LeftMotor_Speed = (short) AllMotor_Parameters[1];
-    LeftPID_Contorller.SetOutputLimits(-LeftMotor_Speed, LeftMotor_Speed);
-    
-    RightPID_Target = (double) AllMotor_Parameters[2];
-    RightMotor_Speed = (short) AllMotor_Parameters[3];
-    RightPID_Contorller.SetOutputLimits(-RightMotor_Speed, RightMotor_Speed);
-    ReadString_Complete = false;
-    ReadString_Input = "";
-  } 
+    else
+    {
+      ReadString_Input += c;
+    }
+  }
+   LeftPID_Target = (double) AllMotor_Parameters[0];
+   LeftMotor_Speed = (double) AllMotor_Parameters[1];
+   LeftPID_Contorller.SetOutputLimits(-LeftMotor_Speed, LeftMotor_Speed);
+   RightPID_Target = (double) AllMotor_Parameters[2];
+   RightMotor_Speed = (double) AllMotor_Parameters[3];
+   RightPID_Contorller.SetOutputLimits(-RightMotor_Speed, RightMotor_Speed);
 }
 
-void motorGo(uint8_t motor, uint8_t direct, uint8_t pwm) 
+void motorAction(uint8_t motor, uint8_t pwm, int PinA_Value, int PinB_Value) 
 {
   if(motor == LeftMotor)
   {
-    if(direct == CW)
-    {
-      digitalWrite(LeftMotor_A1_PIN, LOW); 
-      digitalWrite(LeftMotor_B1_PIN, HIGH);
-    }
-    else if(direct == CCW)
-    {
-      digitalWrite(LeftMotor_A1_PIN, HIGH);
-      digitalWrite(LeftMotor_B1_PIN, LOW);      
-    }
-    else
-    {
-      digitalWrite(LeftMotor_A1_PIN, LOW);
-      digitalWrite(LeftMotor_B1_PIN, LOW);            
-    }
-    
-    analogWrite(LeftMotor_PWM, pwm); 
+    digitalWrite(LeftMotor_A1_PIN, PinA_Value); 
+    digitalWrite(LeftMotor_B1_PIN, PinB_Value);
+    analogWrite(LeftMotor_PWM, pwm);
   }
   else if(motor == RightMotor)
   {
-    if(direct == CW)
-    {
-      // CW input for right motor is actually CCW
-      digitalWrite(RightMotor_A2_PIN, HIGH);
-      digitalWrite(RightMotor_B2_PIN, LOW);
-    }
-    else if(direct == CCW)
-    {
-      // CCW input for right motor is actually CW
-      digitalWrite(RightMotor_A2_PIN, LOW);
-      digitalWrite(RightMotor_B2_PIN, HIGH);      
-    }
-    else
-    {
-      digitalWrite(RightMotor_A2_PIN, LOW);
-      digitalWrite(RightMotor_B2_PIN, LOW);            
-    }
-    
+    digitalWrite(RightMotor_A2_PIN, PinA_Value);
+    digitalWrite(RightMotor_B2_PIN, PinB_Value);
     analogWrite(RightMotor_PWM, pwm);
   }
 }
 
-void motorPIDControl(volatile long *encoderValue, double *setPoint, double *output, PID *motorPID, uint8_t EN_PIN, uint8_t Motor) 
+void PID_Calculation(double *output, PID *motorPID, uint8_t motor) 
 {
     motorPID->Compute();
-
     if (*output > 0) 
-    {
-      motorGo(Motor, CW, *output);
-    }
-    else if (*output < 0)
-    {
-      motorGo(Motor, CCW, abs(*output));
-    }
-    if (*encoderValue == *setPoint) {
-      
-      motorGo(Motor, BRAKE, 0);
-      *output = 0;
-    }
+      motorAction(motor, *output, HIGH, LOW);
+    else if (*output < 0) 
+      motorAction(motor, abs(*output), LOW, HIGH);
+    else if (*output == 0)
+      motorAction(motor, 0, LOW, LOW);
 }
 
-void LeftSetup()
+void LeftInitialSetting()
 {
   // Left setup (Motor，Rotary Encoder，PID)
   pinMode(LeftMotor_A1_PIN, OUTPUT);
@@ -215,8 +168,7 @@ void LeftEncoder_Update()
   LeftEncoder_LastValue = encoded; //store this value for next time
 }
 
-
-void RightSetup()
+void RightInitialSetting()
 {
   // Right setup (Motor，Rotary Encoder，PID)
   pinMode(RightMotor_A2_PIN, OUTPUT);
