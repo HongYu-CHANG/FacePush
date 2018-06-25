8e63ba7fed7b9844a428547906317d02c6f38123
RedFishCHANG <redfishchang..cs06g@nctu.edu.tw>
2018-06-25 22:43:52 +0800

    Start try add driver controll code to new one

---
+++ DC MOTOR/Applicaton/For Siggraph/Diving Experience/Assets/Scripts/DiverControll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiverControll : MonoBehaviour {
+    
+    //Diver Body
+    public GameObject LeftHand;
+    public GameObject RightHand;
+
+    // Use this for initialization
+    void Start ()
+    {
+        transform.position = new Vector3((LeftHand.transform.position.x + RightHand.transform.position.x) / 2, 
+            0.47f, (LeftHand.transform.position.z + RightHand.transform.position.z) / 2);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
