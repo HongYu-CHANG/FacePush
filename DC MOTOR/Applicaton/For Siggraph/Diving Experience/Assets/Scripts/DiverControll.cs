1e9a9a95b3363eecc98e0369ac6473f5ee96455b
RedFishCHANG <redfishchang..cs06g@nctu.edu.tw>
2018-07-04 20:26:12 +0800

    修改轉彎 把轉向改得比較滑順

--- DC MOTOR/Applicaton/For Siggraph/Diving Experience/Assets/Scripts/DiverControll.cs
+++ DC MOTOR/Applicaton/For Siggraph/Diving Experience/Assets/Scripts/DiverControll.cs
@@ -17,7 +17,6 @@ public class DiverControll : MonoBehaviour {
     private Vector3 RlastPos;//右手上一次的位置
     private Vector3 Lvector;
     private Vector3 Rvector;
-    private Vector3 driverFace;
     private Vector3 diveDirection; //身體的方向
 
     //v2: 用合力計算旋轉角度 & 移動
@@ -25,7 +24,10 @@ public class DiverControll : MonoBehaviour {
     private float body_vector_angle;
     private int i = 0;
     private float offset = 1;
-    private bool rotated = false;
+    private bool isRotated = false;
+    private bool isR_Rotated = false;
+    private bool isL_Rotated = false;
+    private Vector3 rotateValue = Vector3.zero;
 
     //
     public float drawRayTime = 10;
@@ -35,84 +37,98 @@ public class DiverControll : MonoBehaviour {
     {
         this.transform.position = new Vector3((driverLeftHand.position.x + driverRightHand.position.x) / 2,
             0.47f, (driverLeftHand.position.z + driverRightHand.position.z) / 2);
+        
     }
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate()
     {
-        Lvector = LlastPos - driverLeftHand.position;
-        Rvector = RlastPos - driverRightHand.position;
+        Lvector = new Vector3(LlastPos.x - driverLeftHand.position.x, 0, LlastPos.z - driverLeftHand.position.z);
+        Rvector = new Vector3(RlastPos.x - driverRightHand.position.x, 0, RlastPos.z - driverRightHand.position.z);
+        //Rvector = RlastPos - driverRightHand.position;
         diveDirection = directionContorl.position - driver.position;
-        //Debug.Log(Lvector);
-        //Debug.Log(Rvector);
-        //Debug.DrawRay(LeftHand.position, Lvector, Color.red, drawRayTime);
-        //Debug.DrawRay(RightHand.position, Rvector, Color.red, drawRayTime);
-        //Debug.DrawRay(bodypos.transform.position, body_head_direction * 10, Color.red, drawRayTime);
-        
         LRvector = Lvector + Rvector;
         body_vector_angle = Vector3.Angle(new Vector3(diveDirection.x, 0, diveDirection.z), new Vector3(LRvector.x, 0, LRvector.z));
-        Debug.Log(Vector3.Angle(LRvector, diveDirection));
-
+        Debug.Log("RlastPos" + RlastPos);
+        Debug.Log("driverRightHand.position" + driverRightHand.position);
+        //Debug.Log(Mathf.Abs(Rvector.magnitude - Lvector.magnitude));
+        Debug.Log(Rvector);
         //rotation
-        //    if (Rvector.magnitude > 0.05f || Lvector.magnitude > 0.05f)
-        //{
-        //    if (Mathf.Abs(Rvector.magnitude - Lvector.magnitude) > 0.08f && LRvector.x > 0)
-        //    {
-        //        if (Rvector.magnitude < Lvector.magnitude)
-        //        {
-        //            //L > R: turn right
-        //                transform.Rotate(Vector3.up * body_vector_angle * 0.08f);
-        //            //if (rotated == false && fish_control.fish == 0 && shark_control.shark == 0)
-        //            //{
-        //            //    StartCoroutine(No1Work(true, false, 0, 0)); //R,L,angle,speed (OSC)
-        //            //    new Thread(Uno.SendData).Start("20 150 120 150"); // L Lspeed R Rspeed
-        //            //    Debug.Log("turn right, L motor");
-        //            //}
+        if (Rvector.magnitude > 0.05f || Lvector.magnitude > 0.05f)
+        {   
+            //if (Mathf.Abs(Rvector.magnitude - Lvector.magnitude) > 0.08f && LRvector.x > 0)
+           // {
+                if ((Lvector.magnitude - Rvector.magnitude) > 0.08f) //trun right
+                {
+                    //Debug.LogWarning("Right!!");
+                    isR_Rotated = true;
+                    isL_Rotated = false;
+                    //Debug.Log("Right" + Vector3.up * body_vector_angle * 0.03f);
+                    rotateValue = Vector3.up * body_vector_angle * 1.2f;
+                    //transform.Rotate(Vector3.up * body_vector_angle * 0.03f);
+                    //if (isRotated == false && fish_control.fish == 0 && shark_control.shark == 0)
+                    //{
+                    //    StartCoroutine(No1Work(true, false, 0, 0)); //R,L,angle,speed (OSC)
+                    //    new Thread(Uno.SendData).Start("20 150 120 150"); // L Lspeed R Rspeed
+                    //    Debug.Log("turn right, L motor");
+                    //}
 
-        //        }
-        //        else
-        //        {
-        //            //R > L: turn left
-        //                transform.Rotate(Vector3.down * body_vector_angle * 0.08f);
-        //            //if (rotated == false && fish_control.fish == 0 && shark_control.shark == 0)
-        //            //{
-        //            //    StartCoroutine(No1Work(false, true, 0, 0));
-        //            //    Debug.Log("turn left, R motor");
-        //            //}
+                }
+                else if ((Rvector.magnitude - Lvector.magnitude) > 0.08f)//turn left
+                {
+                    //Debug.LogWarning("Left!!");
+                    isR_Rotated = false;
+                    isL_Rotated = true;
+                    //Debug.Log("Left" + Vector3.down * body_vector_angle * 0.03f);
+                    rotateValue = Vector3.down * body_vector_angle * 1.2f;
+                    //transform.Rotate(Vector3.down * body_vector_angle * 0.03f);
+                    //if (isRotated == false && fish_control.fish == 0 && shark_control.shark == 0)
+                    //{
+                    //    StartCoroutine(No1Work(false, true, 0, 0));
+                    //    Debug.Log("turn left, R motor");
+                    //}
 
-        //        }
+                }
+                else
+                {
+                    //Debug.LogWarning("Forward!!");
+                    rotateValue = Vector3.zero;
+                }
 
-        //        rotated = true;
-        //    }
-        //    else
-        //    {
-        //        Debug.Log("don't rotate");
-        //        rotated = false;
-        //    }
+                //isRotated = true;
+            //}
+        }
 
-        //}
-
-        //offset control
+        //drive offset control
         if (LRvector.magnitude > 0.01f && LRvector.magnitude < 0.02f) offset += 0.1f;
-            else if (LRvector.magnitude > 0.02f && LRvector.magnitude < 0.03f) offset += 0.2f;
-            else if (LRvector.magnitude > 0.03f && LRvector.magnitude < 0.04f) offset += 0.3f;
-            else if (LRvector.magnitude> 0.04f && LRvector.magnitude < 0.05f) offset += 0.4f;
-            else if (LRvector.magnitude > 0.05f) offset += 0.5f;
+        else if (LRvector.magnitude > 0.02f && LRvector.magnitude < 0.03f) offset += 0.2f;
+        else if (LRvector.magnitude > 0.03f && LRvector.magnitude < 0.04f) offset += 0.3f;
+        else if (LRvector.magnitude> 0.04f && LRvector.magnitude < 0.05f) offset += 0.4f;
+        else if (LRvector.magnitude > 0.05f) offset += 0.5f;
 
-            if (offset > 12) offset = 12;
-            //if (Input.GetKeyDown(KeyCode.Q)) { Debug.Log("offset: " + offset); StartCoroutine(No1Work(false, false, 0, 255)); }
+        if (offset > 12) offset = 12;
+        //if (Input.GetKeyDown(KeyCode.Q)) { Debug.Log("offset: " + offset); StartCoroutine(No1Work(false, false, 0, 255)); }
 
-            //swim forward
-                if (rotated)
-            {
-                transform.position += new Vector3(diveDirection.x, 0, diveDirection.z) * (LRvector.magnitude + offset) * 0.05f;// 0.0005f ;
-                transform.position += new Vector3(diveDirection.x, 0, diveDirection.z) * (LRvector.magnitude + offset) * 0.005f * Time.deltaTime;
-                offset = offset * 0.2f;//* 0.005f;//0.0005f;//
-                Debug.Log("---rotate---");
-            }
-            else//straight
-            {       
-                transform.position += new Vector3(diveDirection.x, 0, diveDirection.z) * (LRvector.magnitude + offset) * 2.2f * Time.deltaTime;
+        if (isRotated)//swim Right rotation
+        {
+            transform.Rotate(rotateValue * Time.deltaTime);
+            rotateValue -= rotateValue * Time.deltaTime;
+            ////transform.position += new Vector3(diveDirection.x, 0, diveDirection.z) * (LRvector.magnitude + offset) * 0.05f;// 0.0005f ;
+            //transform.position += new Vector3(diveDirection.x, 0, diveDirection.z) * (LRvector.magnitude + offset) * 0.055f * Time.deltaTime;
+            //offset = offset * 0.2f;//* 0.005f;//0.0005f;//
+            Debug.Log("---rotate---" + rotateValue);
+            Debug.Log("---rotate---" + rotateValue.magnitude);
+        }
+        else//straight
+        {
+            transform.position += new Vector3(diveDirection.x, 0, diveDirection.z) * (LRvector.magnitude + offset) * 2.2f * Time.deltaTime;
+            transform.Rotate(rotateValue * Time.deltaTime);
+            rotateValue -= rotateValue * Time.deltaTime;
+            ////transform.position += new Vector3(diveDirection.x, 0, diveDirection.z) * (LRvector.magnitude + offset) * 0.05f;// 0.0005f ;
+            //transform.position += new Vector3(diveDirection.x, 0, diveDirection.z) * (LRvector.magnitude + offset) * 0.055f * Time.deltaTime;
+            //offset = offset * 0.2f;//* 0.005f;//0.0005f;//
+            //Debug.Log("---rotate---" + rotateValue);
+            //Debug.Log("---rotate---" + rotateValue.magnitude);
 
             /*fish & shark & motor control
                 if (fish_control.fish == 1) Debug.Log("fish!!");
@@ -137,17 +153,17 @@ public class DiverControll : MonoBehaviour {
 
         }
 
-            //reset
-            LlastPos = driverLeftHand.position;
-            RlastPos = driverRightHand.position;
-            Lvector = Vector3.zero;
-            Rvector = Vector3.zero;
-            counter = 0;
-            if (offset > 0.2f) offset -= 0.2f;
-            else offset = 0;
-            rotated = false;
+         //reset parameter
+         LlastPos = driverLeftHand.position;
+         RlastPos = driverRightHand.position;
+         Lvector = Vector3.zero;
+         Rvector = Vector3.zero;
+         counter = 0;
+         if (offset > 0.2f) offset -= 0.2f;
+         else offset = 0;
+        //if (rotateValue.magnitude < 5) rotateValue = Vector3.zero;//isRotated = false;
 
-            if (motorSegmentCounter == 2) motorSegmentCounter = 0;
-            else motorSegmentCounter++;
+         if (motorSegmentCounter == 2) motorSegmentCounter = 0;
+         else motorSegmentCounter++;
         }
 }
